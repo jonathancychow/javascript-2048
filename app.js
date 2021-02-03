eb632cd9e034f939e45d6f4cf22ddd7012998e69
Jonathan Chow <jonathan.chow@mclaren.com>
2021-02-03 15:47:36 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const gridDisplay = document.querySelector('.grid')
+    const scoreDisplay = document.querySelector('.score')
+    const resultDisplay = document.querySelector('.result')
+    const width = 4
+    let squares = []
+
+    function createBoard(){
+        for(let i=0; i < width * width; i++ ){
+            square = document.createElement('div')
+            square.innerHTML = 0
+            gridDisplay.appendChild(square)
+            squares.push(square)
+        }
+        // generate()
+    }
+    createBoard()
+
+    // function generate(){
+    //     let randomNumber = Math.floor(Math.random * squares.length)
+    //     if (squares[randomNumber].innerHTML == 0){
+    //         squares[randomNumber].innerHTML = 2
+    //     } else generate()
+    // }
+
+)
